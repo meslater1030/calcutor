f442cadd512dd383b35a0e8d309cf85f36c4289f
wesleywooten <wootenwesley@gmail.com>
2015-08-10 13:15:06 -0700

    added Ans functionality

--- calcutor/calcutor/static/calc.js
+++ calcutor/calcutor/static/calc.js
@@ -1,18 +1,36 @@
 $(function(){
+    var output = "";
+    var last_input = "";
     var input = "";
+    var operators = ["/", "*", "+", "-"];
+
+
+    var parse_string = function(string){
+        return string.split("Ans").join(output);
+    };
 
     $("#buttons button:not(.special)").click(function(event){
+        if (operators.indexOf($(this).attr( "id" )) > -1 && input == ""){
+            input = "Ans";
+        };
         input = input.concat( $(this).attr( "id" ) );
         $("#screen .input:last").text(input);
     });
 
     $("#ENTER").click(function(event){
+        if (input == ""){
+            input = last_input;
+        } else {
+            last_input = input;
+        };
+        input = parse_string(input);
         $.ajax({
             type: "POST",
             url: "/",
             data: {input: input}
         }).done(function(response){
-            $("#screen .output:last").text(response.output);
+            output = response.output;
+            $("#screen .output:last").text(output);
             $("#screen").append("<p class='input'></p>");
             $("#screen").append("<p class='output'></p>");
             input = "";
