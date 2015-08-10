3f6ffd5e4ab8ef9b57b01bf8d59363d693a36ab1
Nicholas Draper <ndraper2@gmail.com>
2015-08-10 11:00:07 -0700

    updated renderer for index

--- calcutor/calcutor/views.py
+++ calcutor/calcutor/views.py
@@ -2,7 +2,7 @@ from pyramid.view import view_config
 from scripts import simple_math
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='index.html')
 def my_view(request):
     if request.method == 'POST':
         input = request.params.get('input')
@@ -10,6 +10,6 @@ def my_view(request):
             output = simple_math.evaluate(input)
         except ValueError:
             error_msg = "That isn't a valid calculator input."
-            return {'error': error_msg}
+            return {'output': error_msg}
         return {'output': output}
     return {}
