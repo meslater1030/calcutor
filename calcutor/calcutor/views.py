28a816eb5cd130470f96ab6d90b066b465473b03
Nicholas Draper <ndraper2@gmail.com>
2015-08-10 11:40:18 -0700

    view now correctly returns json

--- calcutor/calcutor/views.py
+++ calcutor/calcutor/views.py
@@ -1,16 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from pyramid.view import view_config
 from scripts import simple_math
 
 
+@view_config(route_name='home', xhr=True, renderer='json')
 @view_config(route_name='home', renderer='templates/index.jinja2')
 def my_view(request):
     if request.method == 'POST':
-        input = request.params.get('input')
+        input = request.params.get('input').decode('utf-8')
         try:
             output = simple_math.evaluate(input)
+            output = unicode(output).encode('utf-8')
         except ValueError:
-            error_msg = "That isn't a valid calculator input."
+            error_msg = b"That isn't a valid calculator input."
             return {'output': error_msg}
-        import pdb; pdb.set_trace()
         return {'output': output}
     return {}
