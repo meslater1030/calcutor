2680fa8cceebf064b173364712220eb234ccdf40
Nicholas Draper <ndraper2@gmail.com>
2015-08-10 10:14:23 -0700

    first pass at simple math

---
+++ calcutor/calcutor/scripts/simple_math.py
@@ -0,0 +1,25 @@
+import ast
+
+
+class Visitor(ast.NodeVisitor):
+    def visit(self, node):
+        if not isinstance(node, self.whitelist):
+            raise ValueError(node)
+        return super(Visitor, self).visit(node)
+
+    whitelist = (
+        ast.Module, ast.Expr, ast.Load, ast.Expression, ast.Add,
+        ast.Sub, ast.UnaryOp, ast.Num, ast.BinOp,
+        ast.Mult, ast.Div, ast.Pow, ast.BitOr, ast.BitAnd,
+        ast.BitXor, ast.USub, ast.UAdd, ast.FloorDiv, ast.Mod,
+        ast.LShift, ast.RShift, ast.Invert
+    )
+
+
+def evaluate(expr):
+    try:
+        node = ast.parse(expr.strip(), mode='eval')
+        Visitor().visit(node)
+        return eval(compile(node, "", "eval"))
+    except:
+        raise ValueError(expr)
--- calcutor/calcutor/views.py
+++ calcutor/calcutor/views.py
@@ -1,6 +1,9 @@
 from pyramid.view import view_config
+from scripts import simple_math
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='json')
 def my_view(request):
-    return {'project': 'calcutor'}
+    input = request.params.get('input')
+    output = simple_math.evaluate(input)
+    return {'output': output}
