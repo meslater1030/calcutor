43204b2b08f58b63111290675b0da4b7eea7a4ce
Nicholas Draper <ndraper2@gmail.com>
2015-08-11 10:45:03 -0700

    regex to filter out **, start of graph equation parsing

--- calcutor/calcutor/views.py
+++ calcutor/calcutor/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from pyramid.view import view_config
-from scripts import fourFn
+from scripts import fourFn, graph_parse
 from pyparsing import ParseException
+import re
+ERROR_MSG = b"That isn't a valid calculator input."
 
 
 @view_config(route_name='home', xhr=True, renderer='json')
@@ -11,16 +13,35 @@ from pyparsing import ParseException
 def my_view(request):
     if request.method == 'POST':
         input = request.params.get('input')
-        for unic, byte in [('\u02c9', '-'), ('\u00B2', '^2')]:
-            input = input.replace(unic, byte)
+        try:
+            input = clean_string(input)
+        except ValueError:
+            return {'output': ERROR_MSG}
         try:
             fourFn.BNF().parseString(input)
             output = fourFn.evaluateStack()
         except ParseException:
-            error_msg = b"That isn't a valid calculator input."
-            return {'output': error_msg}
+            return {'output': ERROR_MSG}
         if float.is_integer(output):
             output = int(output)
         output = unicode(output).encode('utf-8')
         return {'output': output}
     return {}
+
+
+@view_config(route_name='graph', renderer='json')
+def graph_view(request):
+    if request.method == 'POST':
+        input = request.params.get('input')
+        for unic, byte in [('\u02c9', '-'), ('\u00B2', '^2')]:
+            input = input.replace(unic, byte)
+        graph_parse.graph_parse(input)
+
+
+def clean_string(input):
+    if re.search(r'[+\-*/=]{2,}', input):
+        raise ValueError
+    for unic, byte in [('\u02c9', '-'), ('\u00B2', '^2')]:
+        input = input.replace(unic, byte)
+    input = re.sub(r'(\d+)(X)', r'\1 * \2', input)
+    return input
